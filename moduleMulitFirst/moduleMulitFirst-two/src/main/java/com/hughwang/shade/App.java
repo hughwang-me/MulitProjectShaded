2d0e87780906d0028d393bf83c8a1f871d14591d
wanghuan <hunter.v.wang@gmail.com>
2017-10-25 14:02:13 +0800

    push

--- moduleMulitFirst/moduleMulitFirst-two/src/main/java/com/hughwang/shade/App.java
+++ moduleMulitFirst/moduleMulitFirst-two/src/main/java/com/hughwang/shade/App.java
@@ -1,5 +1,10 @@
 package com.hughwang.shade;
 
+import com.hughwang.shade.utils.L;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        L.info("moduleMulitFirst-two App Start !!!");
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+                new String[]{"contextConfiguration.xml"}
+        );
+        applicationContext.start();
+        L.info("dubbo-redis-service Application Started !!!");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        L.info("dubbo-redis-service Application Over !!!");
     }
 }
